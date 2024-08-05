4cf6ef895a667a63754b07a100cf10c4b7b5d1a6
Javier Morales <javiermduarte70@gmail.com>
2024-08-04 22:37:58 -0400

    commit ng init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomepageComponent } from './homepage/homepage.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent,NavbarComponent, HomepageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'galaxylabs';
+}
