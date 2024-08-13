54ecf1feef76c02f7664e0a5885f20edb348ba99
Javier Morales <javiermduarte70@gmail.com>
2024-08-13 19:35:54 -0400

    commit init homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomepageComponent],
+  imports: [RouterOutlet, NavbarComponent, HomepageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, NavbarComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
