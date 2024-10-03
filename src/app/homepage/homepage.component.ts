09d076d8d80739af5d61fb96179b0f5a6ac23567
Javier Morales <javiermduarte70@gmail.com>
2024-10-02 20:42:47 -0400

    updated home content

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -5,15 +5,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, NavbarComponent],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, NavbarComponent,MatFormFieldModule],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+contactForm: any;
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   services = [
     { icon: 'computer', title: 'Software Development', description: 'We offer custom software development services.' },
     { icon: 'settings', title: 'IT Consulting', description: 'Our experts provide top-notch IT consulting services.' },
